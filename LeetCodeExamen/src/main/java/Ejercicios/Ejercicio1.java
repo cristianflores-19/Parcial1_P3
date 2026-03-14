62f6d5960710862d1325171c0726085411e17a3a
Cristian Flores <cfloresp5@miumg.edu.gt>
2026-03-14 14:22:27 -0600

    segundo commit

--- LeetCodeExamen/src/main/java/Ejercicios/Ejercicio1.java
+++ LeetCodeExamen/src/main/java/Ejercicios/Ejercicio1.java
@@ -9,10 +9,34 @@ public class Ejercicio1 {
         int[] ejemplo3 = {5, 5, 5};
 
         
-        System.out.println("Entrada [1, 2, 3, 4, 5] -> Salida: " + score(ejemplo1)); 
-        System.out.println("Entrada [17, 19, 21]    -> Salida: " + score(ejemplo2)); 
-        System.out.println("Entrada [5, 5, 5]       -> Salida: " + score(ejemplo3)); 
+        System.out.println("Entrada [1, 2, 3, 4, 5]  Salida: " + score(ejemplo1)); 
+        System.out.println("Entrada [17, 19, 21]     Salida: " + score(ejemplo2)); 
+        System.out.println("Entrada [5, 5, 5]        Salida: " + score(ejemplo3)); 
 
 	}
+	public static int score(int[] numbers) {
+        int puntajeTotal = 0;
+        
+       
+        if (numbers == null || numbers.length == 0) {
+            return 0;
+        }
+
+        
+        for (int i = 0; i < numbers.length; i++) {
+            int num = numbers[i];
+
+            if (num == 5) {
+                
+                puntajeTotal += 5;
+            } else if (num % 2 == 0) {
+                puntajeTotal += 1;
+            } else {
+                puntajeTotal += 3;
+            }
+        }
+
+        return puntajeTotal;
 	
+	}
 }
