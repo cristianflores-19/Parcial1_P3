5869cd571fb24c95bdaa4a4d5cf8d87e520e963a
Cristian Flores <cfloresp5@miumg.edu.gt>
2026-03-14 14:40:58 -0600

    cuarto commit

--- LeetCodeExamen/src/main/java/Ejercicios/Ejercicio2.java
+++ LeetCodeExamen/src/main/java/Ejercicios/Ejercicio2.java
@@ -2,14 +2,28 @@ package Ejercicios;
 
 public class Ejercicio2 {
 
-	public static void main(String[] args) {
-        int[] ejemplo = {7, 2, 9, 4, 1, 8};
+    public static void main(String[] args) {
         
+        int[] ejemplo = {7, 2, 9, 4, 1, 8};
+        int[] resultado = buscarSegundoMenorYMayor(ejemplo);
         
         System.out.println("Entrada: [7, 2, 9, 4, 1, 8]");
-        System.out.println("Salida: [" + resultado[0] + ", " + resultado[1] + "] (Esperado: [2, 8])");
-    
+        System.out.println("Salida: [" + resultado[0] + ", " + resultado[1] + "]");
+    }
 
-	}
-
+    public static int[] buscarSegundoMenorYMayor(int[] nums) {
+        
+        int menor = nums[0];
+        int segundoMenor = nums[1];
+        
+        int mayor = nums[0];
+        int segundoMayor = nums[1];
+        
+        if (menor > segundoMenor) {
+            menor = nums[1];
+            segundoMenor = nums[0];
+        }
+        
+       
+    }
 }
